2f2ec4367d087961fba5f84d5a800eabfee62177
tahtafa <tahtaccnt@gmail.com>
2025-04-28 01:59:04 +0700

    menambahkan API mahasiswa

---
+++ tpmodul10_103022300142/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿namespace tpmodul10_103022300142.Models
+    
+
+
+{
+    public class Mahasiswa
+    {
+
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+
+        public Mahasiswa(string nama, string nim)
+        {
+            Nama = nama;
+            Nim = nim;
+        }
+    }
+}
---
+++ tpmodul10_103022300142/MahasiswaConntroller.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tpmodul10_103022300142.Models;
+
+
+namespace tpmodul10_103022300142.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        
+        private static List<Mahasiswa> listMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa("M. Tahtafaiz Madani", "103022300142"),
+            new Mahasiswa("Dian Fardiansyah", "1030220300"),
+            new Mahasiswa("Putri Afni Azkiya", "1030220300125"),
+            new Mahasiswa("Fathian Alfiana Rahman", "103022300084"),
+            new Mahasiswa("Nawra Nazli Kirana", "1030220300054")
+            
+        };
+
+        
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return listMahasiswa;
+        }
+
+        
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> Get(int index)
+        {
+            if (index < 0 || index >= listMahasiswa.Count)
+            {
+                return NotFound();
+            }
+            return listMahasiswa[index];
+        }
+
+        
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa mhs)
+        {
+            listMahasiswa.Add(mhs);
+        }
+
+        
+        [HttpDelete("{index}")]
+        public void Delete(int index)
+        {
+            if (index >= 0 && index < listMahasiswa.Count)
+            {
+                listMahasiswa.RemoveAt(index);
+            }
+        }
+    }
+}
